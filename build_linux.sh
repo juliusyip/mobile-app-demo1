7c9e7d3ab99f811e2e1bac4555c861e6ec5426af
charleslcso <520537+charleslcso@users.noreply.github.com>
2021-10-25 13:46:06 +0800

    Add files via upload

---
+++ build_linux.sh
@@ -0,0 +1,29 @@
+node('ci.tourdefarm.com'){
+	stage('For Linux') {
+		sh '''echo Ubuntu build only'''
+	}
+	stage('Preparing: documents, data, directories, ') {
+		sh '''echo stage2 steps!!'''
+	}
+	stage('Prepare context & environment') {
+		sh '''echo stage1 steps!!'''
+	}
+	stage('Checkout source code and related tests') {
+		sh '''echo stage1 steps!'''
+	}
+	stage('Validate environment') {
+		sh '''echo stage1 steps!!'''
+	}
+	stage('Setup database') {
+		sh '''echo stage1 steps!!'''
+	}
+	stage('Run tests') {
+		sh '''echo stage3 steps!!'''
+	}
+	stage('Cleanup') {
+		sh '''echo cleaning up...!!!!'''
+	}
+	stage('Send result') {
+		sh '''echo sending result...!!!!'''
+	}
+}
